3da306053d6f3d33a8170b5d070d8277da4e3a19
eRIZ <przemyslaw.pawliczuk@h2p.pl>
2014-07-12 17:24:22 +0200

    fixed migration index name

--- db/migrate/001_create_pastes.rb
+++ db/migrate/001_create_pastes.rb
@@ -10,7 +10,7 @@ class CreatePastes < ActiveRecord::Migration
     end
     add_index :pastes, :created_at, name: 'index_pastes_on_creation'
     add_index :pastes, :user_id, name: 'index_pastes_on_user'
-    add_index :pastes, :accessed_at, name: 'index_pastes_on_user'
+    add_index :pastes, :accessed_at, name: 'index_pastes_on_accessed'
     add_index :pastes, :project_id, name: 'index_pastes_on_project_id'
   end
 end
